1094cf71c6f878b5fc0a37798f6f5bd3369085fb
MolyCloud <wj442089346@hotmail.com>
2019-08-15 23:44:36 +0800

    初始化

--- src/main/java/com/cskaoyan/sb/springboot_project/controller/LoginAdminController.java
+++ src/main/java/com/cskaoyan/sb/springboot_project/controller/LoginAdminController.java
@@ -1,7 +1,5 @@
 package com.cskaoyan.sb.springboot_project.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
