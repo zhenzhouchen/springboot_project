0f06a6918d331b3c3359d4dff1b2e1449966eb31
zhenzhouchen <you@example.com>
2019-08-16 08:43:57 +0800

    初始化文件

--- src/main/java/com/cskaoyan/sb/springboot_project/SpringbootProjectApplication.java
+++ src/main/java/com/cskaoyan/sb/springboot_project/SpringbootProjectApplication.java
@@ -1,9 +1,11 @@
 package com.cskaoyan.sb.springboot_project;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.cskaoyan.sb.springboot_project.mapper")
 public class SpringbootProjectApplication {
 
     public static void main(String[] args) {
