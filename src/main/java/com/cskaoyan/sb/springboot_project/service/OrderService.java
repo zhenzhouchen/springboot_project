ae436194b251fb11aa802a97f193950063a6d8ab
zhenzhouchen <you@example.com>
2019-08-17 14:44:03 +0800

    统计报表已经完成

--- src/main/java/com/cskaoyan/sb/springboot_project/service/OrderService.java
+++ src/main/java/com/cskaoyan/sb/springboot_project/service/OrderService.java
@@ -1,9 +1,14 @@
 package com.cskaoyan.sb.springboot_project.service;
 
+import com.cskaoyan.sb.springboot_project.bean.Order_statistics;
+
+import java.util.List;
 import java.util.Map;
 
 public interface OrderService {
     Map<String, Object> selectOrderList(int page, int limit, Integer[] orderStatusArray, String sort, String order, Integer userId, String orderSn);
 
     Map<String, Object> selectOrderDetailById(int id);
+
+    List<Order_statistics> query_order_statistics();
 }
