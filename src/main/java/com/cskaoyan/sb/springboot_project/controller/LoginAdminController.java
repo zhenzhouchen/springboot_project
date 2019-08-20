49f7965ff14f1c27b0736451558686d90194753e
MolyCloud <wj442089346@hotmail.com>
2019-08-20 21:35:36 +0800

    处理pom冲突

--- src/main/java/com/cskaoyan/sb/springboot_project/controller/LoginAdminController.java
+++ src/main/java/com/cskaoyan/sb/springboot_project/controller/LoginAdminController.java
@@ -10,7 +10,6 @@ import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
